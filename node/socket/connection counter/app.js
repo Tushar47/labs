e84e3f609eb0319973548d7253b10cace2885b32
Tushar47 <tusharlsonawane@gmail.com>
2013-05-02 19:36:51 +0530

    Errors Corrected

--- node/socket/connection counter/app.js
+++ node/socket/connection counter/app.js
@@ -1,18 +1,19 @@
 var app = require('express')(),
 	server = require('http').createServer(app).listen(8000),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server)
+	activeClients = 0;
 	
-app.get('/', function(request, response){
-  response.sendfile(__dirname + "/index.html");
+app.get('/', function(request, response) {
+	response.sendfile(__dirname + '/index.html');
 });
- 
-var activeClients = 0;
- 
+
 io.sockets.on('connection', function(socket) {
 	activeClients += 1;
+	console.log(activeClients);
 	io.sockets.emit('message', { clients: activeClients });
 	socket.on('disconnect', function() {
 		activeClients -= 1;
+		console.log(activeClients);
 		io.sockets.emit('message', { clients: activeClients });
 	});
 });
